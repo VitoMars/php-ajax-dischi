06c4e32dff986366bba8bb2ca6f8cff96b5122cf
Vito <vito95v@gmail.com>
2021-10-12 17:08:39 +0200

    Fine esercizio

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+include "partials/database.php";
 ?>
 
 <!DOCTYPE html>
@@ -18,8 +19,22 @@
          <img class="img" src="./img/logo.png" alt="" />
       </div>
    </header>
-   <main>
 
+   <main>
+      <div class="song-container">
+         <?php
+         foreach ($db as $song) {
+            echo "<div class='carta'>";
+
+            echo "<img src='$song[poster]'>";
+            echo "<div class='title'> $song[title]</div>";
+            echo "<div class='little-text'> $song[author]</div>";
+            echo "<div class='little-text'> $song[year]</div>";
+
+            echo "</div>";
+         }
+         ?>
+      </div>
    </main>
 </body>
 
